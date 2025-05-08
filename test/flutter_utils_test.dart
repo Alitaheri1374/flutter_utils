4d65a65a7dc65c9d6d3de4478015beee1f1f0433
Ali.Taheri <taheri.alitaheri.ali@gmail.com>
2025-05-08 13:12:43 +0330

    Includes classes or functions in Dart to perform tasks faster in Flutter

--- test/flutter_utils_test.dart
+++ test/flutter_utils_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_utils/flutter_utils.dart';
+// import 'package:flutter_utils/flutter_utils.dart';
 
 void main() {
   // test('adds one to input values', () {
